2b7c4e0b82483cba4367da4cc7092e171da1c8b1
hejin <hejin82@gmail.com>
2018-03-11 14:08:18 +0900

    ch05

--- Express.js-in-Action/ch05/temperature-by-zip/app.js
+++ Express.js-in-Action/ch05/temperature-by-zip/app.js
@@ -1,13 +1,42 @@
 var express = require("express");
+var path = require("path");
+var zipdb = require("zippity-do-dah");
+var ForecastIo = require("forecastio");
 
 var app = express();
+var weather = new ForecastIo("4b04b298450956e057bcbcde1ac48d1f");
 
-app.get("/olivia", function(request, response) {
-    response.send("welcome to Olivia's homepage");
+app.use(express.static(path.resolve(__dirname, "public")));
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get(/^\/(\d{5})$/, function(req, res, next) {
+    var zipcode = req.params[0];
+    var location = zipdb.zipcode(zipcode);
+    if (!location.zipcode) {
+        next();
+        return;
+    }
+    var latitude = location.latitude;
+    var longitude = location.longitude;
+    weather.forecast(latitude, longitude, function(err, data) {
+        if (err) {
+            next();
+            return;
+        }
+        res.json({
+            zipcode: zipcode,
+            temperature: data.currently.temperature
+        });
+    });
 });
 
 app.use(function(request, response) {
-    response.status(404).send("page not found");
+    response.status(404).render("404");
 });
 
 app.listen(3000);
